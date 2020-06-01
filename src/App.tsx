71682024c4854227c2e0a112c420d085e2806244
krushna-sharma <krushna.sharma12162643@gmail.com>
2020-06-01 11:41:14 +0530

    Removed commented code from App.js

--- src/App.tsx
+++ src/App.tsx
@@ -16,28 +16,9 @@ interface IWeatherApiResponse {
 }
 
 function App() {
-	// const [ data, setdata ] = useState([]);
-
-	// useEffect(() => {
-	// 	Api(apiList.GET_WEATHER_DATA.replace('{city_name}', 'Akola'), Method.GET, true)
-	// 		.then((resp: any) => {
-	// 			console.log(resp);
-	// 			let response: IWeatherApiResponse = resp;
-	// 			setdata(response.list);
-	// 		})
-	// 		.catch((err) => {
-	// 			console.log(err);
-	// 		});
 	// }, []);
 
 	return (
-		// <div className="App">
-		// 	{data.map((weatherData: any, index: number) => {
-		// 		return (
-		// 			<img key={index} src={`${apiList.GET_ICON.replace('{icon_name}', weatherData.weather[0].icon)}`} alt="weather_icon" />
-		// 		);
-		// 	})}
-		// </div>
 		<ErrorPage>
 				<BrowserRouter>
 					<Switch>
