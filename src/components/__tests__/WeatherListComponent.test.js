c3a644a82037588327da9e826a7fefd3c5cb00bd
krushna-sharma <krushna.sharma12162643@gmail.com>
2020-06-10 21:42:54 +0530

    Trying to implement testing

---
+++ src/components/__tests__/WeatherListComponent.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {fireEvent,render} from '@testing-library/react';
+
+
+describe('Testing', () => {
+    it('First test', () => {
+        expect(true).toBeTruthy()
+    });
+});
